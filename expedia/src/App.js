3baf0733f7fe48bf55bb9cfd3fbd0ba1c44e3396
Sharadinduprakash <Sharadyadav2786@gmail.com>
2022-05-05 19:57:59 +0530

    add agn

--- expedia/src/App.js
+++ expedia/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
-import ExpediaParent from './Components/ExpediaParent';
+
 
 function App() {
   return (
     <div className="App">
-      <ExpediaParent/>
+      
     </div>
   );
 }
