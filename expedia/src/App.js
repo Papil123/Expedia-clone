1e8a5744b481ee7208fc919d4e78bc4e57ecf8d4
shubhamvts6 <shubhamvts6@gmail.com>
2022-05-06 14:28:56 +0530

    Update App.js

--- expedia/src/App.js
+++ expedia/src/App.js
@@ -1,12 +1,31 @@
 
+// import './App.css';
+// import { Routes,Route } from 'react-router-dom';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react';
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Registration from './Components/Registeration';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="outer">
+        <div className="inner">
+          <Registration />
+        </div>
+      </div>
     </div>
   );
 }
