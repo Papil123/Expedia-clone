f9a3fb2dace9d0064b915c82cd0966dd6def2cdd
Papil Kamdi <papilkamdi786@gmail.com>
2022-05-06 18:32:01 +0530

    app css changed

--- expedia/src/App.js
+++ expedia/src/App.js
@@ -2,10 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
 
+import MainRoutes from './Routes/MainRoutes';
+import {Footer} from './Components/Footer';
+import TabPanel from './Components/TabPanel'
+
 function App() {
   return (
     <div className="App">
-      
+     {/* <Navbar/> */}
+     <TabPanel/>
+     <MainRoutes/>
+     <Footer/>
     </div>
   );
 }
